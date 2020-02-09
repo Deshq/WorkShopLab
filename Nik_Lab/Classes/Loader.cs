8e9953824f3cd9396be7760bad161a0ae34268c0
Denis Nikiforov <den007ik@gmail.com>
2020-02-09 18:10:25 +0200

    third commit: add Loader from txt

--- Nik_Lab/Classes/Loader.cs
+++ Nik_Lab/Classes/Loader.cs
@@ -6,6 +6,44 @@ namespace WorkShop_Nikiforov.Classes
 {
     class Loader
     {
-        
+        public static void LoadRecipeFromTxt(List<Recipe> Recipes)
+        {
+            string path = @"C:\Users\Desh\Desktop\Projects\Nik_Lab\Nik_Lab\Recipes.txt";
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] strlist = line.Split(';');
+
+                    Recipe Recipe_n = new Recipe(strlist[0]);
+
+                    for (int l = 1; l < strlist.Length; l += 2)
+                    {
+                        Item Product = new Item(strlist[l], Convert.ToInt32(strlist[l + 1]));
+                        Recipe_n.MaterialsForRecipe.Add(Product);
+                    }
+
+                    Recipes.Add(Recipe_n);
+                }
+            }
+        }
+        public static void LoadProductsToMaterialsFromTxt(List<Materials> MaterialsInStorage)
+        {
+            string path = @"C:\Users\Desh\Desktop\Projects\Nik_Lab\Nik_Lab\Materials.txt";
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine())
+                    != null)
+                {
+                    string[] strlist = line.Split(';');
+
+                    Materials Material = new Materials(strlist[0], Convert.ToInt32(strlist[1]), Convert.ToInt32(strlist[2]));
+
+                    MaterialsInStorage.Add(Material);
+                }
+            }
+        }
     }
 }
